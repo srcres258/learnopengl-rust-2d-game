d9b43f574936437f6a974e20e18d6265cd4bf342
src_resources <src.res.211@gmail.com>
2024-06-01 22:18:41 +0800

    Add OpenGL debug message output

--- 2d-game/src/main.rs
+++ 2d-game/src/main.rs
@@ -14,7 +14,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::ffi::{c_void, CStr};
 use std::ptr;
+use gl::types::{GLchar, GLenum, GLsizei, GLuint};
 use glfw::{Action, Context, Glfw, Key, Modifiers, OpenGlProfileHint, Scancode, Window, WindowHint};
 use crate::game::Game;
 
@@ -38,6 +40,56 @@ const SCREEN_WIDTH: u32 = 800;
 const SCREEN_HEIGHT: u32 = 600;
 
 static mut GAME_OBJ_PTR: *mut Game = ptr::null_mut();
+
+extern "system" fn gl_debug_output(source: GLenum,
+                                   gltype: GLenum,
+                                   id: GLuint,
+                                   severity: GLenum,
+                                   _length: GLsizei,
+                                   message: *const GLchar,
+                                   _user_param: *mut c_void)
+{
+    if id == 131169 || id == 131185 || id == 131218 || id == 131204 { // ignore these non-significant error codes
+        return;
+    }
+
+    println!("---------------");
+    let message_c_str;
+    unsafe {
+        message_c_str = CStr::from_ptr(message);
+    }
+    let message_str = message_c_str.to_str().unwrap();
+    println!("Debug message ({}): {}", id, message_str);
+    match source {
+        gl::DEBUG_SOURCE_API => println!("Source: API"),
+        gl::DEBUG_SOURCE_WINDOW_SYSTEM => println!("Source: Window System"),
+        gl::DEBUG_SOURCE_SHADER_COMPILER => println!("Source: Shader Compiler"),
+        gl::DEBUG_SOURCE_THIRD_PARTY => println!("Source: Third Party"),
+        gl::DEBUG_SOURCE_APPLICATION => println!("Source: Application"),
+        gl::DEBUG_SOURCE_OTHER => println!("Source: Other"),
+        _ => {}
+    }
+    match gltype {
+        gl::DEBUG_TYPE_ERROR => println!("Type: Error"),
+        gl::DEBUG_TYPE_DEPRECATED_BEHAVIOR => println!("Type: Deprecated Behaviour"),
+        gl::DEBUG_TYPE_UNDEFINED_BEHAVIOR => println!("Type: Undefined Behaviour"),
+        gl::DEBUG_TYPE_PORTABILITY => println!("Type: Portability"),
+        gl::DEBUG_TYPE_PERFORMANCE => println!("Type: Performance"),
+        gl::DEBUG_TYPE_MARKER => println!("Type: Marker"),
+        gl::DEBUG_TYPE_PUSH_GROUP => println!("Type: Push Group"),
+        gl::DEBUG_TYPE_POP_GROUP => println!("Type: Pop Group"),
+        gl::DEBUG_TYPE_OTHER => println!("Type: Other"),
+        _ => {}
+    }
+    match severity {
+        gl::DEBUG_SEVERITY_HIGH => println!("Severity: high"),
+        gl::DEBUG_SEVERITY_MEDIUM => println!("Severity: medium"),
+        gl::DEBUG_SEVERITY_LOW => println!("Severity: low"),
+        gl::DEBUG_SEVERITY_NOTIFICATION => println!("Severity: notification"),
+        _ => {}
+    }
+    println!();
+}
 
 fn main() {
     // glfw: initialize and configure
@@ -68,6 +120,11 @@ fn main() {
     // OpenGL configuration
     // --------------------
     unsafe {
+        gl::Enable(gl::DEBUG_OUTPUT);
+        gl::Enable(gl::DEBUG_OUTPUT_SYNCHRONOUS); // makes sure errors are displayed synchronously
+        gl::DebugMessageCallback(Some(gl_debug_output), ptr::null());
+        gl::DebugMessageControl(gl::DONT_CARE, gl::DONT_CARE, gl::DONT_CARE, 0, ptr::null(), gl::TRUE);
+
         gl::Viewport(0, 0, SCREEN_WIDTH as _, SCREEN_HEIGHT as _);
         gl::Enable(gl::BLEND);
         gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
